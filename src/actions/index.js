9d14a67a78fcac384cbabbc965acf207aa8375c1
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-08-30 19:09:11 -0400

    fetch messages from api

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// TODO: add and export your own actions
+export const FETCH_POSTS = "FETCH_POSTS";
+
+
+export function fetchPosts() {
+  const promise = fetch('http://reduxblog.herokuapp.com/api/posts?key=lala').then(r => r.json());
+
+  return {
+    type: FETCH_POSTS,
+    payload: promise
+  };
+}
---
+++ src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createHistory as history } from 'history';
+
+// importing reducers
+import postsReducer from './reducers/posts_reducer';
+
+// Import components
+import PostsIndex from './containers/posts_index';
+
+import '../assets/stylesheets/application.scss';
+
+const reducers = combineReducers({
+  posts: postsReducer
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, {}, middlewares)}>
+    <Router history={history}>
+      <div className="thin-container">
+        <Switch>
+          <Route path="/" exact component={PostsIndex} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
